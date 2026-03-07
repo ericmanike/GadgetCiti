6585cae10c92b3366989232a78bb8ecffe51adfc
Ericsson <manikeeric@gmail.com>
2026-03-07 21:54:13 +0000

    login

--- components/Profiledropdown.tsx
+++ components/Profiledropdown.tsx
@@ -31,7 +31,7 @@ export default function DropdownProfile() {
   if (!user) {
     return (
       <button
-        onClick={() => router.push('/auth/login')}
+        onClick={() => router.push('/auth/Login')}
         className="px-4 py-2 bg-orange-500 text-white rounded-lg text-sm font-bold hover:bg-orange-600 transition"
       >
         Sign In
