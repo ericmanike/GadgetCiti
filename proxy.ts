bdf7c4df756f44209d617ee52198e0b575d74ff9
Ericsson <manikeeric@gmail.com>
2026-03-07 23:55:03 +0000

    login

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get('token')
   console.log('-------------------------')
   console.log("Middleware token is:", token);
