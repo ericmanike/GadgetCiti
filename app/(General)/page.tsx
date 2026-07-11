293b5eb23de97ecf1381f94b7a4aebd2b81a0651
Ericsson <manikeeric@gmail.com>
2026-07-11 02:04:15 +0000

    smartphones fixed

--- app/(General)/page.tsx
+++ app/(General)/page.tsx
@@ -71,12 +71,12 @@ export default function Home() {
 
             {/* Small Category Cards */}
             {[
-              { title: 'Laptops', url: '/cat_laptops.png', query: 'laptops' },
+              { title: 'Laptops', url: '/L1.jpg', query: 'laptops' },
          
               { title: 'Latest Smartphones', url: '/cat_smartphones.png', query: 'iphones' },
-              { title: 'Tablets', url: '/cat_tablets.png', query: 'tablets' },
-              { title: 'Accessories', url: '/cat_accessories.png', query: 'accessories' },
-             { title: 'Printers', url: '/cat_printers.png', query: 'printers' },
+              { title: 'Tablets & Ipad', url: '/cat_tablets.png', query: 'tablets' },
+              { title: 'Accessories & Audio', url: '/cat_accessories.png', query: 'accessories' },
+             { title: 'Printers & Scanners', url: '/cat_printers.png', query: 'printers' },
               { title: 'IT Infrastructure', url: '/cat_hardware.png', query: 'hardware & IT infrastructure' },
             ].map((cat, idx) => (
               <Link
